c03836995207922ebbc7475d96a51a78197ccadd
Nama Anda <email@example.com>
2025-01-02 10:03:32 +0700

    (+) instalasi & setup template and entry point

---
+++ config/cors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'paths' => ['api/*'],
+	'allowed_methods' => ['*'],
+	'allowed_origins' => ['*'], // Sesuaikan dengan domain yang diizinkan
+	'allowed_headers' => ['*'],
+	'supports_credentials' => false,
+];
